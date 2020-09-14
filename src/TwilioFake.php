52d23ff5fd2ac07b369803c8b8543e939d00ac6c
Robert Kerr <robert.kerr@elevationeg.com>
2020-09-14 12:00:58 -0500

    make fakes a little better

--- src/TwilioFake.php
+++ src/TwilioFake.php
@@ -2,31 +2,39 @@
 
 namespace Rksugarfree\Twilio;
 
-use Rksugarfree\Twilio\Interfaces\CommunicationsClient;
 use Twilio\Rest\Api;
 use Twilio\Rest\Api\V2010;
 use Twilio\Rest\Api\V2010\Account\CallInstance;
 use Twilio\Rest\Api\V2010\Account\MessageInstance;
 use Twilio\Rest\Client;
 
-class TwilioFake implements CommunicationsClient
+class TwilioFake extends Twilio
 {
     public function call(string $to, $message, array $params = []): CallInstance
     {
-        return new CallInstance($this->v2010ApiClient(), [], '');
+        return new CallInstance($this->v2010ApiClient(), [
+            'to' => $to,
+            'from' => $params['from'] ?? $this->from,
+        ], $this->sid);
     }
 
     public function message(string $to, string $message, array $mediaUrls = [], array $params = []): MessageInstance
     {
-        return new MessageInstance($this->v2010ApiClient(), [], '');
+        return new MessageInstance($this->v2010ApiClient(), [
+            'to' => $to,
+            'from' => $params['from'] ?? $this->from,
+        ], $this->sid);
+    }
+
+    public function getClient(): Client
+    {
+        return new Client('nonsense', 'nonsense');
     }
 
     private function v2010ApiClient(): V2010
     {
-        $dummyClient = new Client('nonsense', 'nonsense');
-
         return new V2010(
-            new Api($dummyClient)
+            new Api($this->getClient())
         );
     }
 }
